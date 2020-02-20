7c3a3880cb9bc47fe1de38874d93d0915844bd54
Vu.Tuan.Giang <giangvt-0992>
2020-02-20 17:15:38 +0700

    add missing column

---
+++ database/migrations/2020_02_20_084221_add_avatar_to_artists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAvatarToArtistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('artists', function (Blueprint $table) {
+            $table->string('avatar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('artists', 'avatar')) {
+            Schema::table('artists', function (Blueprint $table) {
+                $table->dropColumn('avatar');
+            });
+        }
+    }
+}
