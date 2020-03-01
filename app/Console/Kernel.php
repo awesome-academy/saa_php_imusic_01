438b71bc2e9fcf4b9cc3bca4703905902172a670
Vu.Tuan.Giang <giangvt-0992>
2020-03-01 18:34:19 +0700

    delete lyric monthly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DeleteLyricCommand;
+use App\Repositories\LyricRepository;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        DeleteLyricCommand::class
     ];
 
     /**
@@ -24,8 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('lyric:delete_monthly')
+                 ->monthly();
     }
 
     /**
